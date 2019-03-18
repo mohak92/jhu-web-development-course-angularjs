f0d830c1d27ffb48d933a83a65ec555bda0dd39a
Mohak Tamhane <mohaktamhane@Mohaks-MacBook-Pro.local>
2019-03-18 06:51:03 -0400

    bug fixes

--- module5-solution/src/common/common.module.js
+++ module5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://sheltered-gorge-34968.herokuapp.com')
+.constant('ApiPath', 'https://sudhirs-coursera.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
